8941387bef40f25fc97f9e67dd1e4b628dc89784
Nathanaël Martel <nat@nathanaelmartel.net>
2018-11-09 20:52:04 +0100

    password generator

--- generate/password/index.php
+++ generate/password/index.php
@@ -1,4 +1,11 @@
-<!DOCTYPE html>
+<?php
+
+require_once($_SERVER['DOCUMENT_ROOT'].'/vendor/nathanaelmartel/password.class.php');
+use NathanaelMartel\Password\Password;
+
+$password = new Password();
+
+?><!DOCTYPE html>
 <html lang="fr">
 
   <head>
@@ -14,6 +21,55 @@
         <section id="content">
 			<h1>Générateur de mots de passe</h1>
 
+            <form action="" method="POST">
+                <fieldset>
+                    <legend>Génerer un mot de passe</legend>
+                    <p>
+                        <label for="length">Longueur</label>
+                        <input type="number" name="length" id="length" value="<?php echo (isset($_POST['length']))?$_POST['length']:15; ?>" min="4" max="255" required />
+                    </p>
+                    <p>
+                        <label for="strength">Caracèteres</label>
+                        <select name="strength" id="strength">
+                            <option value="0" <?php if (isset($_POST['strength']) && ($_POST['strength'] == 0)): ?>selected="selected"<?php endif ?>>Lettres minuscules</option>
+                            <option value="1" <?php if (isset($_POST['strength']) && ($_POST['strength'] == 1)): ?>selected="selected"<?php endif ?>>Lettres minuscules et majuscules</option>
+                            <option value="2" selected="selected">Lettres et chiffres</option>
+                            <option value="3" <?php if (isset($_POST['strength']) && ($_POST['strength'] == 3)): ?>selected="selected"<?php endif ?>>Lettres, chiffres et caractères spéciaux</option>
+                        </select>
+                    </p>
+                    <p>
+                        <input type="submit" value="Génerer" />
+                    </p>
+                    <?php if (isset($_POST['length']) && isset($_POST['strength'])): ?>
+                        <p>
+                            <strong><?php echo $password->generate($_POST['length'], $_POST['strength']) ?></strong>
+                        </p>
+                    <?php endif ?>
+                </fieldset>
+            </form>
+
+            <div class="col-flex">
+                <ul>
+                    <li><?php echo $password->generate(8, 1) ?></li>
+                    <li><?php echo $password->generate(8, 1) ?></li>
+                    <li><?php echo $password->generate(8, 1) ?></li>
+                    <li><?php echo $password->generate(8, 1) ?></li>
+                </ul>
+
+                <ul>
+                    <li><?php echo $password->generate() ?></li>
+                    <li><?php echo $password->generate() ?></li>
+                    <li><?php echo $password->generate() ?></li>
+                    <li><?php echo $password->generate() ?></li>
+                </ul>
+
+                <ul>
+                    <li><?php echo $password->generate(18, 3) ?></li>
+                    <li><?php echo $password->generate(18, 3) ?></li>
+                    <li><?php echo $password->generate(18, 3) ?></li>
+                    <li><?php echo $password->generate(18, 3) ?></li>
+                </ul>
+            </div>
 	    </section>
 
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/_footer.php') ?>
---
+++ vendor/nathanaelmartel/password.class.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace NathanaelMartel\Password;
+
+class Password {
+
+    public function generate($length = 15, $strength = 2)
+    {
+        $vowels = 'aeuy';
+        $consonants = 'bdghjmnpqrstvz';
+
+        if ($strength > 0) {
+            $consonants .= strtoupper($consonants);
+            $vowels .= strtoupper($vowels);
+        }
+
+        if ($strength > 1) {
+            $vowels .= '23456789';
+        }
+
+        if ($strength > 2) {
+            $vowels .= '@#$%';
+        }
+
+        $password = '';
+        $alt = time() % 2;
+        for ($i = 0; $i < $length; $i++) {
+            if ($alt == 1) {
+                $password .= $consonants[hexdec(bin2hex(openssl_random_pseudo_bytes(5))) % strlen($consonants)-1];
+                $alt = 0;
+            } else {
+                $password .= $vowels[hexdec(bin2hex(openssl_random_pseudo_bytes(5))) % strlen($vowels)-1];
+                $alt = 1;
+            }
+        }
+
+        return $password;
+    }
+}
