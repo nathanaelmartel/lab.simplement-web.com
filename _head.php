36b290adb74c602cd67df99406185e609ad0d2b2
Nathanaël Martel <nat@nathanaelmartel.net>
2018-11-12 23:19:43 +0100

    CSP first try

--- _head.php
+++ _head.php
@@ -17,56 +17,56 @@
 	<!-- «Vivre tous simplement pour que tous puissent simplement vivre.» Gandhi -->
 
 
-    <script type="application/ld+json">
-    {
-      "@context": "http://schema.org",
-      "@type": "Organization",
-      "name": "simplement Web",
-      "url": "https://www.simplement-web.com/",
-      "logo": "https://www.simplement-web.com/assets/images/logo.png",
-      "contactPoint": {
-        "@type": "ContactPoint",
-        "telephone": "+33781948086",
-        "contactType": "customer service",
-        "areaServed": "FR",
-        "availableLanguage": "French"
-      },
-      "founders": [
+        <script type="application/ld+json">
         {
-            "@type": "Person",
-            "name": "Nathanaël Martel",
-            "email": "mailto:nathanael@simplement-web.com",
-            "image": "https://www.simplement-web.com/assets/images/janvier2015-byHenri-2.jpg",
+          "@context": "http://schema.org",
+          "@type": "Organization",
+          "name": "simplement Web",
+          "url": "https://www.simplement-web.com/",
+          "logo": "https://www.simplement-web.com/assets/images/logo.png",
+          "contactPoint": {
+            "@type": "ContactPoint",
             "telephone": "+33781948086",
-            "url": "https://nathanaelmartel.net",
-            "sameAs": [
-              "https://www.simplement-web.com/",
-              "https://nathanaelmartel.net/",
-              "https://www.linkedin.com/in/nathana%C3%ABl-martel-15299753",
-              "https://twitter.com/simplementNat",
-              "https://github.com/nathanaelmartel",
-              "http://fr.viadeo.com/fr/profile/nathanael.martel"
+            "contactType": "customer service",
+            "areaServed": "FR",
+            "availableLanguage": "French"
+          },
+          "founders": [
+            {
+                "@type": "Person",
+                "name": "Nathanaël Martel",
+                "email": "mailto:nathanael@simplement-web.com",
+                "image": "https://www.simplement-web.com/assets/images/janvier2015-byHenri-2.jpg",
+                "telephone": "+33781948086",
+                "url": "https://nathanaelmartel.net",
+                "sameAs": [
+                  "https://www.simplement-web.com/",
+                  "https://nathanaelmartel.net/",
+                  "https://www.linkedin.com/in/nathana%C3%ABl-martel-15299753",
+                  "https://twitter.com/simplementNat",
+                  "https://github.com/nathanaelmartel",
+                  "http://fr.viadeo.com/fr/profile/nathanael.martel"
+                ]
+            }]
+        }
+        </script>
+
+
+        <script type="application/ld+json">
+        {
+          "@context": "http://schema.org",
+          "@type": "BreadcrumbList",
+          "itemListElement": [{
+          <?php $item = 1; foreach ($breadcrumb as $item_url => $item_name): ?>
+                "@type": "ListItem",
+                "position": <?php echo $item++ ?>,
+                "name": "<?php echo $item_name ?>",
+                "item": "https://www.simplement-web.com<?php echo $item_url ?>"
+            }<?php echo ($item < count($breadcrumb)+1)?',{':''; ?>
+          <?php endforeach ?>
             ]
-        }]
-    }
-    </script>
-
-
-    <script type="application/ld+json">
-    {
-      "@context": "http://schema.org",
-      "@type": "BreadcrumbList",
-      "itemListElement": [{
-      <?php $item = 1; foreach ($breadcrumb as $item_url => $item_name): ?>
-            "@type": "ListItem",
-            "position": <?php echo $item++ ?>,
-            "name": "<?php echo $item_name ?>",
-            "item": "https://www.simplement-web.com<?php echo $item_url ?>"
-        }<?php echo ($item < count($breadcrumb)+1)?',{':''; ?>
-      <?php endforeach ?>
-        ]
-    }
-    </script>
+        }
+        </script>
 
 
 
@@ -74,20 +74,7 @@
 
 
 <!-- Matomo -->
-<script type="text/javascript">
-  var _paq = _paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(["setDoNotTrack", true]);
-  _paq.push(["disableCookies"]);
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="https://stats.simplement-web.com/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', '11']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
+<script src="https://stats.simplement-web.com/piwik.js" async defer></script>
+<script src="/assets/js/tracking.js"></script>
 <noscript><p><img src="https://stats.simplement-web.com/piwik.php?idsite=11&amp;rec=1" style="border:0;" alt="" /></p></noscript>
 <!-- End Matomo Code -->
